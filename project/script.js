2139fe9faaf7cbef83becbc7b804cd5651641f49
Kishan-Mogaveera <kishanmogaveera7@gmail.com>
2026-07-23 10:21:40 +0530

    update

--- project/script.js
+++ project/script.js
@@ -4,9 +4,9 @@ document.querySelector('.site-footer p').innerHTML=
     `&copy;${year}Kishan Chandan. All rights reserved`;
 
 function getGreeting(){
-    const hour=new Date().getHours;
-    if(hour<12) return "Good Morning";
-    if(hour<17) return "Good Afternoon";
+    const hour=new Date().getHours();
+    if(hour < 12) return "Good Morning";
+    if(hour < 17) return "Good Afternoon";
     return "Good Evening";
 }
 const heroTitle =document.querySelector('.hero-section h1');
